b67dc6ec63460ff2c7fc965ad42870986b03f44b
Giuseppe <giusscos@icloud.com>
2026-07-18 22:50:49 +0200

    Initial Commit

---
+++ ShopSurvivors/ShopSurvivorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSurvivorsApp.swift
+//  ShopSurvivors
+//
+//  Created by Giuseppe Cosenza on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShopSurvivorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
